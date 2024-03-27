f8ad576742d63fca136e9f6daba32e32f902e3d1
lucasmarchd01 <lucasmarchd01@gmail.com>
2024-03-27 18:32:17 -0400

    reorganize data

--- data_processing.py
+++ data_processing.py
@@ -28,9 +28,57 @@ def process_csv_files(folder_path):
     return combined_data
 
 
+def generate_unique_frames_across_videos(frame_column):
+    output = []
+    prev_frame = 0
+    next_index = 0
+
+    for frame in frame_column:
+        if frame != prev_frame:
+            next_index += 1
+        output.append(next_index)
+        prev_frame = frame
+
+    return output
+
+
+def flatten_data(data):
+    data = pd.read_csv("combined_data_unique_frames.csv")
+    data["Landmark_Hand"] = data["Landmark"] + "_" + data["Hand"]
+    pivot_data = data.pivot(
+        index="Unique_Frame",
+        columns="Landmark_Hand",
+        values=["X", "Y", "Z", "Fingering"],
+    )
+
+    # Flatten the MultiIndex columns
+    pivot_data.columns = [
+        f"{col[1]}_{col[0]}" if col[0] != "Fingering" else "Fingering"
+        for col in pivot_data.columns
+    ]
+
+    pivot_data = pivot_data.loc[:, ~pivot_data.columns.duplicated()]
+    pivot_data.reset_index(inplace=True)
+
+    return pivot_data
+
+
 # Path to the folder containing CSV files
 folder_path = "landmark_data_stable"
 
+# Combine data to one csv file
 combined_data = process_csv_files(folder_path)
 combined_data.to_csv("combined_data_filtered.csv", index=False)
 print("Combined data saved to 'combined_data_filtered.csv'")
+
+
+# Apply the function to generate unique frame numbers across videos
+data = pd.read_csv("combined_data_filtered.csv")
+data["Unique_Frame"] = generate_unique_frames_across_videos(data["Frame"])
+data.to_csv("combined_data_unique_frames.csv", index=False)
+
+
+# Save to CSV
+data = pd.read_csv("combined_data_unique_frames.csv")
+data = flatten_data(data)
+data.to_csv("flattened_data.csv", index=False)
